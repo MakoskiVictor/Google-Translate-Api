d8c68e4166494e0a0558dd425b19d1d7d384ac75
MakoskiVictor <makoski.ed@gmail.com>
2023-04-11 16:58:57 -0300

    delete build

--- build/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AUTO_LANGUAGE = exports.SUPPORTED_LANGUAGES = void 0;
-exports.SUPPORTED_LANGUAGES = {
-    en: 'English',
-    es: 'Español',
-    de: 'Deutsch'
-};
-exports.AUTO_LANGUAGE = 'auto';
