9ed731ea8567347c1db70426a2c09814ce5ab22c
MakoskiVictor <makoski.ed@gmail.com>
2023-04-11 07:31:31 -0300

    added translate route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import { translate } from "../controllers/translate";
 
 const router = express.Router()
 
 router.get("/", (_req, res) => {
     res.send("Backend of Google Translate Api")
+})
+
+router.get("/translate", (req, res) => {
+    try {
+        const [fromLanguage, toLanguage, text] = req.body
+        res.send(translate({fromLanguage, toLanguage, text}))        
+    } catch (error) {
+      res.send(error)  
+    }
 })
 
 export default router
