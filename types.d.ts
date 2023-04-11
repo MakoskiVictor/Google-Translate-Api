28ba478879c215b5610573b92e992f800f53cb2d
MakoskiVictor <makoski.ed@gmail.com>
2023-04-11 07:31:05 -0300

    added Props types

---
+++ types.d.ts
@@ -0,0 +1,31 @@
+import { type } from "os"
+import { SUPPORTED_LANGUAGES, AUTO_LANGUAGE } from "./constants"
+
+export interface State {
+    fromLanguage: FromLanguage
+    toLanguage: string
+    fromText: string
+    result: string
+    loading: boolean
+  }
+  
+  export type Action =
+    | { type: 'INTERCHANGE_LANGUAGES' }
+    | { type: 'SET_FROM_LANGUAGE', payload: FromLanguage }
+    | { type: 'SET_TO_LANGUAGE', payload: Language }
+    | { type: 'SET_FROM_TEXT', payload: string }
+    | { type: 'SET_RESULT', payload: string }
+  
+  // keyof typeof => el tipo es una de las keys del objeto supported_languages
+  export type Language = keyof typeof SUPPORTED_LANGUAGES
+  
+  export type AutoLanguage = typeof AUTO_LANGUAGE
+  
+  export type FromLanguage = Language | AutoLanguage
+
+  export type Props = {
+    fromLanguage: FromLanguage
+    toLanguage: Language
+    text: string
+  }
+  
