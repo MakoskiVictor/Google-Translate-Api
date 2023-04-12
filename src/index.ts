f297cd4ed8226e85e8be220c66aa0bcb2e934735
MakoskiVictor <makoski.ed@gmail.com>
2023-04-11 23:27:56 -0300

    added cors reques

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express"
 import routes from "./routes/index"
+import cors from "cors"
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
