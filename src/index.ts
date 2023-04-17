243c16f40d960a605e10cd9bcd6bd03dd99ee508
MakoskiVictor <makoski.ed@gmail.com>
2023-04-17 19:44:29 -0300

    Access-Control-Allow-Headers

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://google-translate-clonned.vercel.app');
-    /* res.header('Access-Control-Allow-Credentials', 'true');
+    /* res.header('Access-Control-Allow-Credentials', 'true'); */
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); */
+    /* res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); */
     next();
   });
 
