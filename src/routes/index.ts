06f1a9d0d66387f5e801bfd9c114c864d2e5d89f
MakoskiVictor <makoski.ed@gmail.com>
2023-04-17 20:21:19 -0300

    Added .then

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,9 +14,10 @@ router.post("/translate", async (req, res) => {
     const {fromLanguage, toLanguage, text} = req.body
     try {
         const response = await translate({fromLanguage, toLanguage, text})
+        .then(response => JSON.stringify(response))
         /* res.setHeader('Content-Type', 'application/json') */
         /* const response = "Hola me llamo Victor desde el back :D" */
-        res.send(JSON.stringify(response))        
+        res.send(response)        
     } catch (error) {
       res.send(error)  
     }
