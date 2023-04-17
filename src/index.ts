13d025d69f8df1a822397fb88c3b3ef143f1e58e
MakoskiVictor <makoski.ed@gmail.com>
2023-04-17 01:21:43 -0300

    allow credentials set to true

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://google-translate-clonned.vercel.app'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'false');
+    res.header('Access-Control-Allow-Origin', 'https://google-translate-clonned.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
