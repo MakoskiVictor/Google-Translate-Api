dd18f553bec3e204fc612ef080a1a3839b794825
MakoskiVictor <makoski.ed@gmail.com>
2023-04-17 19:40:01 -0300

    Quit credentials

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from "express"
 import routes from "./routes/index"
 /* import cors from "cors" */
 import bodyParser from "body-parser"
-import cookieParser from "cookie-parser"
 /* import { WHITELIST } from "./constants" */
 
 const app = express()
@@ -15,12 +14,11 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
-app.use(cookieParser());
 app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://google-translate-clonned.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    /* res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); */
     next();
   });
 
