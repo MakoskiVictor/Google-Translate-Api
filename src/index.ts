2fb7359af6eaa6581c376565dc0ce557266e50ee
MakoskiVictor <makoski.ed@gmail.com>
2023-04-16 23:47:42 -0300

    open cors

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,25 @@ import express from "express"
 import routes from "./routes/index"
 import cors from "cors"
 import bodyParser from "body-parser"
-import { WHITELIST } from "./constants"
+/* import { WHITELIST } from "./constants" */
 
 const app = express()
 
-app.use(cors({
+/* app.use(cors({
     origin: WHITELIST,
     optionsSuccessStatus: 200
-}))
+})) */
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
-/* app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', `${WHITELIST}`); // update to match the domain you will make the request from
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-  }); */
+  });
 
 const PORT = process.env.PORT || 3000
 
