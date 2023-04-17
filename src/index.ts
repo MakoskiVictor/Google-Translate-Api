193f95a09e9cd01f0b22ccdddaed5a9e1b7c433f
MakoskiVictor <makoski.ed@gmail.com>
2023-04-16 23:21:51 -0300

    change cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express from "express"
 import routes from "./routes/index"
-import cors from "cors"
+/* import cors from "cors" */
 import { WHITELIST } from "./constants"
 
 const app = express()
 
-app.use(cors({
+/* app.use(cors({
     origin: WHITELIST,
     optionsSuccessStatus: 200
-}))
+})) */
 app.use(express.json())
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', `${WHITELIST}`); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 const PORT = process.env.PORT || 3000
 
