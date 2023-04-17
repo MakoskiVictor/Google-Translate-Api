113d0ee554c0d1f25ef47e0575f0b934c5a3186a
MakoskiVictor <makoski.ed@gmail.com>
2023-04-16 23:29:57 -0300

    use open cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import routes from "./routes/index"
-/* import cors from "cors" */
-import { WHITELIST } from "./constants"
+import cors from "cors"
+/* import { WHITELIST } from "./constants" */
 
 const app = express()
 
@@ -9,14 +9,15 @@ const app = express()
     origin: WHITELIST,
     optionsSuccessStatus: 200
 })) */
+app.use(cors())
 app.use(express.json())
-app.use((_req, res, next) => {
+/* app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', `${WHITELIST}`); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-  });
+  }); */
 
 const PORT = process.env.PORT || 3000
 
